11958505564653d6a0e46b03f8bbfd7e243f2c9e
Charith Sankalpa <84385440+Charith-cs@users.noreply.github.com>
2024-10-01 21:33:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use(cors({
     origin: ["https://real-time-chat-swift-front.vercel.app"],
     methods: ["POST", "GET", "OPTIONS"],
     credentials: true,
-    allowedHeaders: ["Content-Type", "application/json"]
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 app.use(helmet());
