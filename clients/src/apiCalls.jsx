8463dc7abd56f1dad69234a4073e5d358f5a5174
Charith Sankalpa <84385440+Charith-cs@users.noreply.github.com>
2024-10-01 21:53:03 +0530

    Update apiCalls.jsx

--- clients/src/apiCalls.jsx
+++ clients/src/apiCalls.jsx
@@ -1,12 +1,18 @@
 import axios from "axios";
 
-export const loginCall = async(userCredentials , dispatch) => {
-    dispatch({type:"LOGIN_START"});
-    try{
-        const res = await axios.post("/api/auth/login" , userCredentials);
-        dispatch({type:"LOGIN_SUCCESS" , payload:res.data});
-    }catch(error){
-        const errorMessage = error.response ? error.response.data.message : "An error occurred during login.";
-        dispatch({ type: "LOGIN_FAILURE", payload: errorMessage });
-    }
+const BASE_URL = process.env.NODE_ENV === "production"
+  ? "https://real-time-chat-swift-back.vercel.app/api"
+  : "/api";  // In development, this will still work with the Vite proxy
+
+export const loginCall = async (userCredentials, dispatch) => {
+  dispatch({ type: "LOGIN_START" });
+  try {
+    const res = await axios.post(`${BASE_URL}/auth/login`, userCredentials);
+    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+  } catch (error) {
+    const errorMessage = error.response
+      ? error.response.data.message
+      : "An error occurred during login.";
+    dispatch({ type: "LOGIN_FAILURE", payload: errorMessage });
+  }
 };
