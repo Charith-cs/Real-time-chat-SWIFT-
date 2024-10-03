2c04f8a9f38c8f6944c71f4f61cebbcd0ae46ebc
Charith Sankalpa <84385440+Charith-cs@users.noreply.github.com>
2024-10-03 21:06:56 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -26,6 +26,7 @@ app.use(cors({
   credentials: true,                                       
   allowedHeaders: ["Content-Type", "Authorization"],        
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
